9abdb749bb30209b319da43a668bec69a629b4fd
root <root@aktus-Inspiron-N5110.(none)>
2012-03-16 13:37:25 +0100

    aktus

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+
+gem 'sqlite3'
+
+
+group :development do
+	gem 'rspec-rails', '~> 2.8'
+end
+group :test do
+	gem 'rspec','~> 2.8' 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '669d970bce068015e97391d67c8f9dfdf0f5c4d945824b34311e9605e593f6f6c3b76fde5fe4bdb0d0594ac223694ab346f8f9ca2d44f96a7b91bb1f906cbd24'
